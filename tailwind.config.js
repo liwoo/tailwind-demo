278342f2baac40b385f96cc5abef1ccd927ec584
Jeremiah Chienda <jeremiahchienda@gmail.com>
2021-11-20 14:10:50 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      blue: {
+        light: "#85d7ff",
+        DEFAULT: "#0075FF",
+        dark: "#009eeb",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
